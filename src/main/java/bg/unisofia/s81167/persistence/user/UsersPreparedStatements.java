dd95b315b9b810b619ed6eda8f17e24917d9d2b5
mnitchev <marionitchev@gmail.com>
2018-01-22 13:13:57 +0200

    Add primary key constraint in Users CREATE_TABLE prepared statement.

--- src/main/java/bg/unisofia/s81167/persistence/user/UsersPreparedStatements.java
+++ src/main/java/bg/unisofia/s81167/persistence/user/UsersPreparedStatements.java
@@ -1,7 +1,7 @@
 package bg.unisofia.s81167.persistence.user;
 
 public enum UsersPreparedStatements {
-    CREATE_TABLE("CREATE TABLE %s (username VARCHAR(25), password VARCHAR(64))"),
+    CREATE_TABLE("CREATE TABLE %s (username VARCHAR(25) PRIMARY KEY, password VARCHAR(64))"),
     GET_USER("SELECT * FROM %s WHERE username = ?"),
     INSERT_USER("INSERT INTO %s VALUES (?, ?)");
 
