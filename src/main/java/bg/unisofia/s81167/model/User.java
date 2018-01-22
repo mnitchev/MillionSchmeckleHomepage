1c42b8ce7f38693417bbfffcd1532f2ff4d5bf6e
mnitchev <marionitchev@gmail.com>
2018-01-22 12:00:08 +0200

    Add hashcode and equals to User.

--- src/main/java/bg/unisofia/s81167/model/User.java
+++ src/main/java/bg/unisofia/s81167/model/User.java
@@ -1,15 +1,41 @@
 package bg.unisofia.s81167.model;
 
+import java.util.Objects;
+
 public class User {
 
     private String username;
+    private String password;
 
-    public User(String username) {
+    public User(String username, String password) {
         this.username = username;
+        this.password = password;
     }
 
     public String getUsername(){
         return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(this.username, other.username)
+                && Objects.equals(this.password, other.password);
     }
 
 }
